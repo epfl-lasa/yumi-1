12f23886a3afd609ac28ed95719fe86327268304
Sina Mirrazavi <sina.mirrazavi@epfl.ch>
2019-01-28 11:49:27 +0100

    .

--- yumi_hw/src/yumi_gripper_rws_node.cpp
+++ yumi_hw/src/yumi_gripper_rws_node.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 #include <ros/console.h>
 
-#include <yumi_hw/yumi_gripper_node.h>
+#include <yumi_hw/yumi_gripper_rws_node.h>
 
 
 /*! Main method of the yumi_rws_gripper_node file. (testing doxygen)
@@ -10,11 +10,14 @@
 int main( int argc, char* argv[] )
 {
     ros::init(argc, argv, "yumi_gripper_node");
-    YumiGripperNode gripperNode;
-    ros::AsyncSpinner spinner(4); // Use 4 threads
-    spinner.start();
-    ros::waitForShutdown();
+    YumiGripperRWSNode gripperNode;
 
+    ros::Rate r(25);
+    while(ros::ok()) {
+	gripperNode.publish();
+	ros::spinOnce();
+	r.sleep();
+    }
     return 0;
 }
 
